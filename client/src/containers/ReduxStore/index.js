45ff021c87a09f5fd0ea156728d874eef34d91d8
carl peaslee <cpeaslee@gmail.com>
2016-11-16 09:38:05 -0600

    a period

--- client/src/containers/ReduxStore/index.js
+++ client/src/containers/ReduxStore/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import {Provider} from 'react-redux'
 import store from '../../store'
-import Routing from './Routing'
+import Routing from '../Routing'
 
 class ReduxStore extends Component {
   render() {
